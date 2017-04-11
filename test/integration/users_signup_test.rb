f721a2482cf5b17bb28c2a875ca0f9ae741db982
Ayushka Partohap <partohap.ayushka@gmail.com>
2017-04-11 23:36:55 +0700

    Finish user sign up

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,37 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+
+  test "invalid signup information" do
+    get signup_path
+
+    assert_no_difference 'User.count' do
+      assert_select "form:match('action',?)", "/signup" 
+      post signup_path, params: {user: {name: "",
+                                       email: "foo@bar",
+                                       password: "foo",
+                                       password_confirmation: "bar"}}
+    end
+
+    assert_template 'users/new'
+    assert_select 'div#error_explanation', /The form contains \d error(s)?/ 
+    assert_select 'div.field_with_errors', 8
+  end
+
+  test "valid signup information" do
+    get signup_path
+    
+    assert_difference 'User.count', 1 do
+      post signup_path, params: {user: {name: "foo",
+                                        email: "foo@bar.net",
+                                        password: "123456",
+                                        password_confirmation: "123456"
+                                       }
+                                }
+    end
+
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.empty?
+  end
+end
